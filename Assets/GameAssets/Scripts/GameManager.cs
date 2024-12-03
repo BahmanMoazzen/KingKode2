43476ec8a4dec565ae83a706637a5b211d963d91
Helen Game Factory <helengamefactory@gmail.com>
2024-12-03 03:46:36 +0330

    Camera Fitting Implemented

--- Assets/GameAssets/Scripts/GameManager.cs
+++ Assets/GameAssets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    
+    List<GameObject> _cardsGameObjects = new List<GameObject>();
+    Camera _camera;
+
     IEnumerator Start()
     {
+
         GameSettings.Initialize();
 
         for (int i = 0; i < GameSettings.Instance.XDimension; i++)
@@ -15,21 +17,35 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < GameSettings.Instance.YDimension; j++)
             {
                 yield return 0;
-                CreateCard(i, j);
-
+                _cardsGameObjects.Add(CreateCard(i, j));
+                _resizeCamera();
             }
         }
-    }
-    
 
-    public void CreateCard(int iXindex, int iYIndex)
+
+    }
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+
+    GameObject CreateCard(int iXindex, int iYIndex)
     {
         GameObject GO = new GameObject();
         GO.AddComponent<CardController>()._LoadCard(GameSettings.Instance.GetRandomCard());
         GO.transform.position = new Vector3(iXindex * GameSettings.Instance.XOffset
             , iYIndex * GameSettings.Instance.YOffset);
-        
 
+        return GO;
+    }
+    void _resizeCamera()
+    {
+        Vector3 oCamCenter = Vector3.zero;
+        float oCamSize = 0;
+        (oCamCenter, oCamSize) = GameSettings.Instance.CalculateCamSize(_cardsGameObjects.ToArray());
+        _camera.transform.position = oCamCenter;
+        _camera.orthographicSize = oCamSize;
     }
 
 }
--- Assets/GameAssets/Scripts/Helpers/GameSettings.cs
+++ Assets/GameAssets/Scripts/Helpers/GameSettings.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 [CreateAssetMenu(fileName = "NewGameSettings", menuName = "BAHMAN/Task2/New Game Settings", order = 0)]
 public class GameSettings : ScriptableObject
 {
     private static GameSettings _instance;
-
+    public int DefaultCameraWidth = 480;
+    public int DefaultCameraHeight = 800;
     [Range(2, 6)]
     public int XDimension = 2;
     [Range(3, 6)]
@@ -16,6 +18,8 @@ public class GameSettings : ScriptableObject
     public float XOffset = .5f;
     [Range(0, 2)]
     public float YOffset = .5f;
+    [Range(0, 2)]
+    public float CameraBuffer = 1f;
     public CardInfo[] AllCards;
     public Vector2 DefaultColliderSize;
     public Sprite CardBackSprite;
@@ -30,16 +34,33 @@ public class GameSettings : ScriptableObject
     {
         return AllCards[Random.Range(0, DeckVariation)];
     }
+    public (Vector3 oCenter, float oSize) CalculateCamSize(GameObject[] iGameCards)
+    {
+        var bounds = new Bounds();
+
+        foreach (var iGameCard in iGameCards)
+        {
+            bounds.Encapsulate(iGameCard.GetComponent<Collider2D>().bounds);
+        }
+
+        bounds.Expand(CameraBuffer);
+        var vertical = bounds.size.y;
+        var horizontal = bounds.size.x * (DefaultCameraHeight / DefaultCameraWidth);
+        var size = Mathf.Max(horizontal, vertical);
+        var center = bounds.center + new Vector3(0, 0, -10);
+        return (center, size);
+    }
     public static GameSettings Instance
     {
         get
         {
-            
+
             return _instance;
         }
 
     }
-    
+
+
 
 
 }
